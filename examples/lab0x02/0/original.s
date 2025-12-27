e47395eaeb55a5c4da5b37d60353a216a060c33c
Teodor B <teobsn@gmail.com>
2025-12-27 03:42:33 +0200

    init

---
+++ examples/lab0x02/0/original.s
@@ -0,0 +1,19 @@
+.data
+    str1: .ascii "ASC"
+    str2: .ascii "FMI"
+
+.text
+
+.global main
+
+main:
+    mov $4, %eax
+    mov $1, %ebx
+    mov $str1, %ecx
+    mov $5, %edx
+    int $0x80
+
+et_exit:
+    mov $1, %eax
+    mov $0, %ebx
+    int $0x80
